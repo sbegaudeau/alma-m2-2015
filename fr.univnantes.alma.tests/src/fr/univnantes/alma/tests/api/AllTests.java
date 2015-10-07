68ce3bab3a879589e0c80ddc6b595016727f4192
Stéphane Bégaudeau <stephane.begaudeau@gmail.com>
2015-10-08 01:30:50 +0200

    Update the project with the full build

---
+++ fr.univnantes.alma.tests/src/fr/univnantes/alma/tests/api/AllTests.java
@@ -0,0 +1,13 @@
+package fr.univnantes.alma.tests.api;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import fr.univnantes.alma.tests.internal.TestClass;
+
+@RunWith(Suite.class)
+@SuiteClasses({TestClass.class})
+public class AllTests {
+
+}
---
+++ fr.univnantes.alma.tests/src/fr/univnantes/alma/tests/internal/TestClass.java
@@ -0,0 +1,12 @@
+package fr.univnantes.alma.tests.internal;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestClass {
+	@Test
+	public void testSomething() {
+		assertTrue(true);
+	}
+}
